36c9f539d53dafa272d74e01dbc992dbe7d2027b
Nurudeen Abdul-Majeed <abdulmajeednurudeen48@gmail.com>
2024-01-29 13:19:57 -0800

    Added append method to the LinkedList class

--- linkedList.js
+++ linkedList.js
@@ -1,10 +1,27 @@
 class LinkedList {
-  constructor() {}
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.nextNode) {
+      current = current.nextNode;
+    }
+    current.nextNode = newNode;
+  }
 }
 
 class Node {
-  constructor(value, nextNode) {
-    this.value = null;
-    this.nextNode = null;
+  constructor(value, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
   }
 }
