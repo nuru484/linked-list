e3934fea632be9e8943d816844a01f3975795d94
Nurudeen Abdul-Majeed <abdulmajeednurudeen48@gmail.com>
2024-01-31 16:44:13 -0800

    Added comments
    o explain the purpose and functionality of each method and class.

--- linkedList.js
+++ linkedList.js
@@ -1,58 +1,75 @@
+// Node class represents a single node in the linked list
+class Node {
+  constructor(value, nextNode = null) {
+    this.value = value; // Value stored in the node
+    this.nextNode = nextNode; // Reference to the next node in the sequence
+  }
+}
+
+// LinkedList class represents the linked list data structure
 class LinkedList {
   constructor() {
-    this.head = null;
+    this.headNode = null; // Reference to the first node in the list (head)
   }
 
+  // Append a new node with the given value to the end of the list
   append(value) {
     const newNode = new Node(value);
 
-    if (!this.head) {
-      this.head = newNode;
+    if (!this.headNode) {
+      this.headNode = newNode; // If list is empty, new node becomes the head
       return;
     }
 
-    let current = this.head;
+    let current = this.headNode;
     while (current.nextNode) {
-      current = current.nextNode;
+      current = current.nextNode; // Traverse the list until the last node
     }
-    current.nextNode = newNode;
+    current.nextNode = newNode; // Append the new node at the end
   }
 
+  // Prepend a new node with the given value to the beginning of the list
   prepend(value) {
-    const newNode = new Node(value, this.head);
-    this.head = newNode;
+    const newNode = new Node(value, this.headNode); // New node points to the current head
+    this.headNode = newNode; // New node becomes the head of the list
   }
 
+  // Get the number of nodes in the list
   size() {
     let total = 0;
-    let current = this.head;
+    let current = this.headNode;
 
     while (current) {
       total++;
-      current = current.nextNode;
+      current = current.nextNode; // Traverse the list
     }
     return total;
   }
 
+  // Get the first node in the list
   getHead() {
-    return this.head;
+    return this.headNode;
   }
+
+  // Get the last node in the list
   tail() {
     let last = null;
-    let current = this.head;
+    let current = this.headNode;
 
     while (current) {
       if (current.nextNode === null) {
         last = current;
       }
-      current = current.nextNode;
+      current = current.nextNode; // Traverse the list
     }
     return last;
   }
+
+  // Get the node at the specified index
   at(index) {
     let node = null;
     let total = 0;
-    let current = this.head;
+    let current = this.headNode;
 
     while (current) {
       total++;
@@ -60,105 +77,111 @@ class LinkedList {
         node = current;
         break;
       }
-      current = current.nextNode;
+      current = current.nextNode; // Traverse the list until reaching the index
     }
     return node;
   }
 
+  // Remove and return the last node from the list
   pop() {
-    let current = this.head;
+    let current = this.headNode;
 
     if (!current) {
       return;
     }
 
     if (!current.nextNode) {
-      this.head = null;
+      this.headNode = null; // If only one node, set head to null
       return current;
     }
 
     while (current.nextNode.nextNode) {
-      current = current.nextNode;
+      current = current.nextNode; // Traverse the list until the second-to-last node
     }
 
     let poppedNode = current.nextNode;
-    current.nextNode = null;
+    current.nextNode = null; // Remove the last node from the list
     return poppedNode;
   }
 
+  // Check if the list contains a node with the given value
   contains(value) {
-    let current = this.head;
+    let current = this.headNode;
 
     if (!current) {
-      return false;
+      return false; // If list is empty, return false
     }
 
     if (current.value === value) {
-      return true;
+      return true; // If head node matches the value, return true
     }
 
     while (current.nextNode) {
       current = current.nextNode;
       if (current.value === value) {
-        return true;
+        return true; // Traverse the list until finding the value
       }
     }
 
     return false;
   }
 
+  // Find the index of the first occurrence of the node with the given value
   find(value) {
-    let current = this.head;
+    let current = this.headNode;
     let index = 0;
 
     if (!current) {
-      return -1;
+      return -1; // If list is empty, return -1
     }
 
     if (current.value === value) {
-      return index;
+      return index; // If head node matches the value, return its index
     }
 
     while (current.nextNode) {
       current = current.nextNode;
       index += 1;
       if (current.value === value) {
-        return index;
+        return index; // Traverse the list until finding the value and return its index
       }
     }
 
-    return -1;
+    return -1; // Return -1 if value is not found in the list
   }
+
+  // Convert the list to a string representation
   toString() {
     let string = '(';
-    let current = this.head;
+    let current = this.headNode;
 
     if (!current) {
-      return '()';
+      return '()'; // If list is empty, return empty string
     }
 
     string += current.value;
 
     while (current.nextNode) {
       string += ') -> (' + current.nextNode.value;
-      current = current.nextNode;
+      current = current.nextNode; // Traverse the list to append each node's value
     }
 
     string += ')';
     return string;
   }
 
+  // Insert a new node with the given value at the specified index
   insertAt(value, index) {
     if (index < 0) {
-      return false;
+      return false; // Return false for invalid index
     }
 
     if (index === 0) {
-      this.prepend(value);
+      this.prepend(value); // If index is 0, prepend the value
       return true;
     }
 
-    let current = this.head;
+    let current = this.headNode;
     let previous = null;
     let currentIndex = 0;
 
@@ -169,31 +192,32 @@ class LinkedList {
     }
 
     if (currentIndex < index) {
-      return false;
+      return false; // If index is out of bounds, return false
     }
 
     const newNode = new Node(value);
     newNode.nextNode = current;
 
     if (previous) {
-      previous.nextNode = newNode;
+      previous.nextNode = newNode; // Insert the new node at the specified index
     } else {
-      this.head = newNode;
+      this.headNode = newNode;
     }
 
     return true;
   }
 
+  // Remove and return the node at the specified index
   removeAt(index) {
     if (index < 0 || index >= this.size()) {
-      return null;
+      return null; // Return null for invalid index
     }
 
     if (index === 0) {
-      return this.pop();
+      return this.pop(); // If index is 0, remove the first node
     }
 
-    let current = this.head;
+    let current = this.headNode;
     let previous = null;
     let currentIndex = 0;
 
@@ -203,16 +227,9 @@ class LinkedList {
       currentIndex++;
     }
 
-    previous.nextNode = current.nextNode;
+    previous.nextNode = current.nextNode; // Remove the node at the specified index
 
     return current.value;
-  }
-}
-
-class Node {
-  constructor(value, nextNode = null) {
-    this.value = value;
-    this.nextNode = nextNode;
   }
 }
 
